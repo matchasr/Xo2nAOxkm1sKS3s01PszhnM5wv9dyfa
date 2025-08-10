8866d53f9943fc6a69437faae63e7982d4074600
matchasr <yuto10sian@gmail.com>
2025-08-10 21:40:15 +0900

    Update loadLists.js

--- loadLists.js
+++ loadLists.js
@@ -3,10 +3,10 @@ const loadPlaylistAmbienceButton = document.getElementById("loadPlaylistAmbience
 
 function loadPlaylistASMR() {
   loadPlaylistASMRButton.style.display = "none";
-  url1.innerHTML += '<iframe class="frame" width="966" height="543" src="https://www.youtube-nocookie.com/embed/videoseries?si=n7YEAseGMWjuGjm2&amp;list=PLZnhlo4OYILrFBirDDYFQv9QeUkdZXVH4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+  url1.innerHTML += '<iframe class="frame" width="483" height="271.5" src="https://www.youtube-nocookie.com/embed/videoseries?si=n7YEAseGMWjuGjm2&amp;list=PLZnhlo4OYILrFBirDDYFQv9QeUkdZXVH4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
 }
 
 function loadPlaylistAmbience() {
   loadPlaylistAmbienceButton.style.display = "none";
-  url2.innerHTML += '<iframe class="frame" width="966" height="543" src="https://www.youtube-nocookie.com/embed/videoseries?si=wbHXpDp2NOH4FdQP&amp;list=PLZnhlo4OYILr8yGZMS2dbKPiLJPpdJldB" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
+  url2.innerHTML += '<iframe class="frame" width="483" height="271.5" src="https://www.youtube-nocookie.com/embed/videoseries?si=wbHXpDp2NOH4FdQP&amp;list=PLZnhlo4OYILr8yGZMS2dbKPiLJPpdJldB" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
 }
