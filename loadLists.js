e51297f8745cca6ef77f75cb1fd74f51285af585
matchasr <yuto10sian@gmail.com>
2026-02-21 18:05:21 +0900

    Add files via upload

--- loadLists.js
+++ loadLists.js
@@ -1,62 +1,44 @@
-const loadPlaylistASMRButton = document.getElementById("loadPlaylistASMR");
-const loadPlaylistAmbienceButton = document.getElementById("loadPlaylistAmbience");
-const loadPlaylistDreamButton = document.getElementById("loadPlaylistDream");
-const loadPlaylistBGMButton = document.getElementById("loadPlaylistBGM");
-
-let loadButton = [];
-for (let i = 1; i < 5; i++) {
-  loadButton[i] = document.getElementById("loadPlaylistBtn-" + i);
-}
-
-
-function reDefineElements() {
-  for (let i = 1; i < 5; i++) {
-    loadButton[i] = document.getElementById("loadPlaylistBtn-" + i);
-  }
-}
-
 const frameElementsNoCookie = [
-  '<iframe class="frame visible-frame" id="frame-1" width="483" height="271.5" src="https://www.youtube-nocookie.com/embed/videoseries?si=H1PtjtGNn_5K2Hl3&amp;list=PL_5OGYEKv3Xkk52A6RN4PafPXA9Vsb4J_&mute=1&fs=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
-  '<iframe class="frame visible-frame" id="frame-2" width="483" height="271.5" src="https://www.youtube-nocookie.com/embed/videoseries?si=c3u6QpLR-Cq4RT1m&amp;list=PL_5OGYEKv3XnBZCIG65gkb49G-Bs_fhqT&mute=1&fs=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
-  '<iframe class="frame visible-frame" id="frame-3" width="483" height="271.5" src="https://www.youtube-nocookie.com/embed/videoseries?si=U0R3m0_IhjBzd_JF&amp;list=PL_5OGYEKv3XmaxTwU3LxjlSWjGSn_II7t&mute=1&fs=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
-  '<iframe class="frame visible-frame" id="frame-4" width="483" height="271.5" src="https://www.youtube-nocookie.com/embed/videoseries?si=5HUsoxOz9XAdLpc6&amp;list=PL_5OGYEKv3Xn3yUqkzMU2MZqPi8H0k7w1&mute=1&fs=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+  '<iframe class="frame visible-frame" id="frame-1" src="https://www.youtube-nocookie.com/embed/videoseries?si=H1PtjtGNn_5K2Hl3&amp;list=PL_5OGYEKv3Xkk52A6RN4PafPXA9Vsb4J_&mute=1&fs=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+  '<iframe class="frame visible-frame" id="frame-2" src="https://www.youtube-nocookie.com/embed/videoseries?si=c3u6QpLR-Cq4RT1m&amp;list=PL_5OGYEKv3XnBZCIG65gkb49G-Bs_fhqT&mute=1&fs=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+  '<iframe class="frame visible-frame" id="frame-3" src="https://www.youtube-nocookie.com/embed/videoseries?si=U0R3m0_IhjBzd_JF&amp;list=PL_5OGYEKv3XmaxTwU3LxjlSWjGSn_II7t&mute=1&fs=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+  '<iframe class="frame visible-frame" id="frame-4" src="https://www.youtube-nocookie.com/embed/videoseries?si=5HUsoxOz9XAdLpc6&amp;list=PL_5OGYEKv3Xn3yUqkzMU2MZqPi8H0k7w1&mute=1&fs=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
 ];
 
 const frameElementsCookie = [
-  '<iframe class="frame visible-frame" id="frame-1" width="483" height="271.5" src="https://www.youtube.com/embed/videoseries?si=H1PtjtGNn_5K2Hl3&amp;list=PL_5OGYEKv3Xkk52A6RN4PafPXA9Vsb4J_&mute=1&fs=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
-  '<iframe class="frame visible-frame" id="frame-2" width="483" height="271.5" src="https://www.youtube.com/embed/videoseries?si=c3u6QpLR-Cq4RT1m&amp;list=PL_5OGYEKv3XnBZCIG65gkb49G-Bs_fhqT&mute=1&fs=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
-  '<iframe class="frame visible-frame" id="frame-3" width="483" height="271.5" src="https://www.youtube.com/embed/videoseries?si=U0R3m0_IhjBzd_JF&amp;list=PL_5OGYEKv3XmaxTwU3LxjlSWjGSn_II7t&mute=1&fs=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
-  '<iframe class="frame visible-frame" id="frame-4" width="483" height="271.5" src="https://www.youtube.com/embed/videoseries?si=5HUsoxOz9XAdLpc6&amp;list=PL_5OGYEKv3Xn3yUqkzMU2MZqPi8H0k7w1&mute=1&fs=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
+  '<iframe class="frame visible-frame" id="frame-1" src="https://www.youtube.com/embed/videoseries?si=H1PtjtGNn_5K2Hl3&amp;list=PL_5OGYEKv3Xkk52A6RN4PafPXA9Vsb4J_&mute=1&fs=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+  '<iframe class="frame visible-frame" id="frame-2" src="https://www.youtube.com/embed/videoseries?si=c3u6QpLR-Cq4RT1m&amp;list=PL_5OGYEKv3XnBZCIG65gkb49G-Bs_fhqT&mute=1&fs=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+  '<iframe class="frame visible-frame" id="frame-3" src="https://www.youtube.com/embed/videoseries?si=U0R3m0_IhjBzd_JF&amp;list=PL_5OGYEKv3XmaxTwU3LxjlSWjGSn_II7t&mute=1&fs=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+  '<iframe class="frame visible-frame" id="frame-4" src="https://www.youtube.com/embed/videoseries?si=5HUsoxOz9XAdLpc6&amp;list=PL_5OGYEKv3Xn3yUqkzMU2MZqPi8H0k7w1&mute=1&fs=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>'
 ];
 
 let frameElements = frameElementsNoCookie;
 
 
 function loadPlaylist(index) {
-  loadButton[index].style.display = "none";
 
   switch (index) {
     case 1: {
-      url[1].innerHTML += frameElements[0];
-      url[1].innerHTML += '<img src="assets/icons/asmr_transparent.png" class="frame-icon" id="frame-icon-1">';
+      frameContainer[1].innerHTML += frameElements[0];
+      frameContainer[1].innerHTML += '<img src="assets/icons/asmr_transparent.png" class="frame-icon" id="frame-icon-1">';
       break;
     }
 
     case 2: {
-      url[2].innerHTML += frameElements[1];
-      url[2].innerHTML += '<img src="assets/icons/ambience_transparent.png" class="frame-icon" id="frame-icon-2">';
+      frameContainer[2].innerHTML += frameElements[1];
+      frameContainer[2].innerHTML += '<img src="assets/icons/ambience_transparent.png" class="frame-icon" id="frame-icon-2">';
       break;
     }
 
     case 3: {
-      url[3].innerHTML += frameElements[2];
-      url[3].innerHTML += '<img src="assets/icons/dream_transparent.png" class="frame-icon" id="frame-icon-3">';
+      frameContainer[3].innerHTML += frameElements[2];
+      frameContainer[3].innerHTML += '<img src="assets/icons/dream_transparent.png" class="frame-icon" id="frame-icon-3">';
       break;
     }
 
     case 4: {
-      url[4].innerHTML += frameElements[3];
-      url[4].innerHTML += '<img src="assets/icons/bgm.png" class="frame-icon" id="frame-icon-4">';
+      frameContainer[4].innerHTML += frameElements[3];
+      frameContainer[4].innerHTML += '<img src="assets/icons/bgm.png" class="frame-icon" id="frame-icon-4">';
       break;
     }
   }
@@ -64,7 +46,6 @@ function loadPlaylist(index) {
 
 
 function reloadPlaylist(index) {
-  reDefineElements();
   const pl = document.getElementById("frame-" + index);
   const icon = document.getElementById("frame-icon-" + index);
 
--- switchScreen.js
+++ switchScreen.js
@@ -2,11 +2,13 @@ const menu = document.getElementById("menu");
 let hideMenu = 0;
 
 let nowPage = 0 //0,1,2,3,4
+const frameContainer = [];
 const url = [];
 const button = [];
 const playlistGlobal = document.getElementById("playlist-global");
 
 for (let i = 0; i < 5; i++) {
+  frameContainer[i] = document.getElementById("fc-" + i);
   url[i] = document.getElementById("url-" + i);
   button[i] = document.getElementById("focus-" + i);
 }
@@ -15,91 +17,9 @@ for (let i = 0; i < 5; i++) {
 function focusOn(index) {
   nowPage = index;
 
-  url.forEach(elements => elements.style.display = "none");
-  button.forEach(elements => elements.style.opacity = "20%");
+  url.forEach(elements => elements.classList.remove("tab-shown"));
+  button.forEach(elements => elements.classList.remove("tab-button-selected"));
 
-  url[index].style.display = "block";
-  button[index].style.opacity = "100%";
-
-  playlistGlobal.style.visibility = (index == 0) ? "hidden" : "visible";
-}
-
-
-
-
-let swipeStartX = 0;
-let swipeEndX = 0;
-
-window.addEventListener('touchstart', (ev) => { swipeStartX = ev.touches[0].clientX });
-
-window.addEventListener('touchmove', (ev) => { swipeEndX = ev.touches[0].clientX });
-
-window.addEventListener('touchend', () => {
-
-  const diff = swipeEndX - swipeStartX;
-
-  if(swipeEndX == 0) return;
-
-  if (Math.abs(diff) > 50) {
-    if (diff > 0) {
-      if (nowPage == 0) return;
-      focusOn(nowPage - 1);
-
-    } else {
-      if (nowPage == 4) return;
-      focusOn(nowPage + 1);
-
-    }
-  }
-
-  swipeStartX = 0;
-  swipeEndX = 0;
-
-});
-
-
-let wheelEventStep = 0;
-let wheelPowerX = 0
-
-window.addEventListener('wheel', (ev) => {
-  wheelPowerX = Math.floor(Math.abs(Math.pow(ev.deltaX, 25) * Math.pow(0.1, 42)));
-
-  if ((wheelPowerX !== 0) && (wheelEventStep == 0)) {
-
-    console.log("nowPage: " + nowPage);
-    console.log(wheelPowerX);
-
-    wheelEventStep = 1;
-
-    if (ev.deltaX < 0) { //左方向
-      if (nowPage == 0) return;
-      focusOn(nowPage - 1);
-
-    } else {
-      if (nowPage == 4) return;
-      focusOn(nowPage + 1);
-    }
-
-    showMenu();
-  }
-});
-
-
-/*
-setInterval(() => {
-  if (wheelPowerX == 0) wheelEventStep = 0;
-  console.log("wheelPowerX: " + wheelPowerX);
-}, 100);
-*/
-
-
-
-function showMenu() {
-  menu.classList.add("menu-shown");
-  
-  if(hideMenu) clearTimeout(hideMenu);
-
-  hideMenu = setTimeout(() => {
-    menu.classList.remove("menu-shown")
-  }, 1000);
+  url[index].classList.add("tab-shown");
+  button[index].classList.add("tab-button-selected");
 }
